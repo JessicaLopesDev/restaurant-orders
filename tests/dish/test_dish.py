0455d6e0a3ac689111862273ec18aae007606640
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-09-11 15:32:05 -0300

    Requisito 2

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,42 @@
-from src.models.dish import Dish  # noqa: F401, E261, E501
+import pytest
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 2
 def test_dish():
-    pass
+    strogonoff = Dish("Strogonoff de carne", 30.0)
+    strogonoff_2 = Dish("Strogonoff de carne", 30.0)
+    ravioli = Dish("Ravioli", 20.0)
+
+    assert strogonoff.name == "Strogonoff de carne"
+    assert ravioli.name == "Ravioli"
+
+    assert hash(strogonoff) == hash(strogonoff_2)
+    assert hash(strogonoff) != hash(ravioli)
+
+    assert strogonoff.__eq__(strogonoff_2) is True
+    assert strogonoff.__eq__(ravioli) is False
+
+    assert repr(strogonoff) == "Dish('Strogonoff de carne', R$30.00)"
+
+    with pytest.raises(TypeError):
+        Dish(30.0, "Strogonoff de carne")
+    with pytest.raises(ValueError):
+        Dish("Strogonoff de carne", 0)
+
+    strogonoff.add_ingredient_dependency(Ingredient("carne"), 1)
+    strogonoff.add_ingredient_dependency(Ingredient("creme de leite"), 1)
+    strogonoff.add_ingredient_dependency(Ingredient("caldo de carne"), 1)
+
+    assert strogonoff.get_restrictions() == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+        Restriction.ANIMAL_MEAT,
+    }
+
+    assert strogonoff.get_ingredients() == {
+        Ingredient("carne"),
+        Ingredient("creme de leite"),
+        Ingredient("caldo de carne"),
+    }
