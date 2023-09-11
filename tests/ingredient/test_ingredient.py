444a5570881b33374f8ee94476f833dee1c98e10
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-09-11 14:17:39 -0300

    Requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,26 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    chicken = Ingredient("frango")
+    chicken_2 = Ingredient("frango")
+    egg = Ingredient("ovo")
+
+    assert chicken.name == "frango"
+    assert egg.name == "ovo"
+
+    assert chicken.restrictions == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.ANIMAL_DERIVED,
+    }
+    assert egg.restrictions == {
+        Restriction.ANIMAL_DERIVED
+    }
+
+    assert chicken.__eq__(chicken_2) is True
+    assert chicken.__eq__(egg) is False
+
+    assert hash(chicken) == hash(chicken_2)
+    assert hash(egg) != hash(chicken_2)
+    assert repr(chicken) == "Ingredient('frango')"
